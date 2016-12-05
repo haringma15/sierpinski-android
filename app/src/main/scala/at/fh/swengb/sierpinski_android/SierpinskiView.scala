052395add7bfff9f7791d9ddb50dd5a3e3d8d70b
Martin Haring <martin.haring2@edu.fh-joanneum.at>
2016-12-05 21:32:59 +0100

    Version 0.2.1 SubTriangles almost work to depth of 2

--- app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
+++ app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
@@ -11,7 +11,7 @@ import android.view.View
 class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View(context, attrs) {
 
   val paint = new Paint()
-  val depth = 1
+  val depth = 2
 
   override protected def onDraw(canvas: Canvas) {
     super.onDraw(canvas)
@@ -20,7 +20,7 @@ class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View
     val y = Math.sqrt(x * x - x/2 * x/2).toFloat
 
     drawTriangle(canvas, x, y)
-    drawSubTriangle(canvas, depth, x, y)
+    drawSubTriangle(canvas, depth, x/2, x/4, x/4*3, y, y/2)
   }
 
   def drawTriangle(canvas: Canvas, x: Float, y: Float) = {
@@ -29,15 +29,15 @@ class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View
     canvas.drawLine(0, y, x/2, 0, paint)
   }
 
-  def drawSubTriangle(canvas: Canvas, depth: Int, x: Float, y: Float): Unit = {
+  def drawSubTriangle(canvas: Canvas, depth: Int, a: Float, b: Float, c: Float, y1: Float, y2: Float): Unit = {
     if (depth > 0){
-      canvas.drawLine(x/2, y, x/4, y/2, paint)
-      canvas.drawLine(x/2, y, x/4*3, y/2, paint)
-      canvas.drawLine(x/4, y/2, x/4*3, y/2, paint)
+      canvas.drawLine(a, y1, b, y2, paint)
+      canvas.drawLine(b, y2, c, y2, paint)
+      canvas.drawLine(c, y2, a, y1, paint)
       val newDepth = depth - 1
-      val newWidth = 1
-      val newHeight = 1
-      drawSubTriangle(canvas, newDepth, newWidth, newHeight)
+      drawSubTriangle(canvas, newDepth, a/2, b/2, c/2, y1, y2/2*3)
+      drawSubTriangle(canvas, newDepth, a/2*3, b/2*5, c/6*7, y1, y2/2*3)
+      drawSubTriangle(canvas, newDepth, a, b/2*3, c/6*5, y2, y2/2)
     }
   }
 }
