b5dcb42b5b571222cb1aa5cee6e5ba4ff733d92a
Martin Haring <martin.haring2@edu.fh-joanneum.at>
2016-12-05 20:25:27 +0100

    Version 0.1.2 Better Overview Prototype of SubTriangles

--- app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
+++ app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
@@ -11,22 +11,33 @@ import android.view.View
 class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View(context, attrs) {
 
   val paint = new Paint()
+  val depth = 1
 
   override protected def onDraw(canvas: Canvas) {
     super.onDraw(canvas)
 
-    drawTriangle(canvas)
-    drawSubTriangle(canvas)
+    val width = canvas.getWidth
+    val height = Math.sqrt(width * width - width/2 * width/2).toFloat
+
+    drawTriangle(canvas, width, height)
+    drawSubTriangle(canvas, depth, width/2, height/2)
   }
 
-  def drawTriangle(canvas: Canvas) = {
-    canvas.drawLine(0, Math.sqrt(canvas.getWidth * canvas.getWidth - canvas.getWidth/2 * canvas.getWidth/2).toFloat,canvas.getWidth(),Math.sqrt(canvas.getWidth * canvas.getWidth - canvas.getWidth/2 * canvas.getWidth/2).toFloat,paint)
-    canvas.drawLine(canvas.getWidth(), Math.sqrt(canvas.getWidth * canvas.getWidth - canvas.getWidth/2 * canvas.getWidth/2).toFloat, canvas.getWidth/2, 0,paint)
-    canvas.drawLine(0, Math.sqrt(canvas.getWidth * canvas.getWidth- canvas.getWidth/2 * canvas.getWidth/2).toFloat, canvas.getWidth/2, 0,paint)
+  def drawTriangle(canvas: Canvas, width: Float, height: Float) = {
+    canvas.drawLine(0, height, width, height, paint)
+    canvas.drawLine(width, height, width/2, 0, paint)
+    canvas.drawLine(0, height, width/2, 0, paint)
   }
 
-  def drawSubTriangle(canvas: Canvas) = {
-    val counter = 0
-
+  def drawSubTriangle(canvas: Canvas, depth: Int, width: Float, height: Float): Unit = {
+    if (depth > 0){
+      canvas.drawLine(0, depth, depth + 500, depth + 600, paint)
+      canvas.drawLine(0, depth, depth + 500, depth + 600, paint)
+      canvas.drawLine(0, depth, depth + 500, depth + 600, paint)
+      val newDepth = depth - 1
+      val newWidth = 1
+      val newHeight = 1
+      drawSubTriangle(canvas, newDepth, newWidth, newHeight)
+    }
   }
 }
