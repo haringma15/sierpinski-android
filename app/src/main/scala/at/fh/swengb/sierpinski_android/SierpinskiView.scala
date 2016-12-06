60cc4d74dd9f9a8e12b3f609ba0b0730b9f67d4b
Martin Haring <martin.haring2@edu.fh-joanneum.at>
2016-12-06 08:34:15 +0100

    Version 1.0.2 DrawTriangle function implemented

--- app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
+++ app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
@@ -19,26 +19,27 @@ class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View
     val x = canvas.getWidth
     val y = Math.sqrt(x * x - x/2 * x/2).toFloat
 
-    drawSierpinski(canvas, 0, y, x/2, 0, x, y)
+    drawSierpinski(canvas, 0, y+50, x/2, 50, x, y+50)
   }
 
   def drawSierpinski(canvas: Canvas, x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float) = {
-    canvas.drawLine(x1, y1, x2, y2, paint)
-    canvas.drawLine(x2, y2, x3, y3, paint)
-    canvas.drawLine(x3, y3, x1, y1, paint)
-
+    drawTriangle(canvas, x1, y1, x2, y2, x3, y3)
     drawSubTriangle(canvas, depth, (x1 + x2)/2, (y1 + y2)/2, (x1 + x3)/2, (y1 + y3)/2, (x2 + x3)/2, (y2 + y3)/2)
   }
 
   def drawSubTriangle(canvas: Canvas, depth: Int, x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float): Unit = {
     if (depth > 0){
-      canvas.drawLine(x1, y1, x2, y2, paint)
-      canvas.drawLine(x2, y2, x3, y3, paint)
-      canvas.drawLine(x3, y3, x1, y1, paint)
+      drawTriangle(canvas, x1, y1, x2, y2, x3, y3)
       val newDepth = depth - 1
       drawSubTriangle(canvas, newDepth, (x1 + x2)/2 + (x2 - x3)/2, (y1 + y2)/2 + (y2 - y3)/2, (x1 + x2)/2 + (x1 - x3)/2, (y1 + y2)/2 + (y1 - y3)/2, (x1 + x2)/2, (y1 + y2)/2)
       drawSubTriangle(canvas, newDepth, (x3 + x2)/2 + (x2 - x1)/2, (y3 + y2)/2 + (y2 - y1)/2, (x3 + x2)/2 + (x3 - x1)/2, (y3 + y2)/2 + (y3 - y1)/2, (x3 + x2)/2, (y3 + y2)/2)
       drawSubTriangle(canvas, newDepth, (x1 + x3)/2 + (x3 - x2)/2, (y1 + y3)/2 + (y3 - y2)/2, (x1 + x3)/2 + (x1 - x2)/2, (y1 + y3)/2 + (y1 - y2)/2, (x1 + x3)/2, (y1 + y3)/2)
     }
+  }
+
+  def drawTriangle(canvas: Canvas, x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float) ={
+    canvas.drawLine(x1, y1, x2, y2, paint)
+    canvas.drawLine(x2, y2, x3, y3, paint)
+    canvas.drawLine(x3, y3, x1, y1, paint)
   }
 }
