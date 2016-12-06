215ae52e167c259b6af33266fb35218c97ef8202
Martin Haring <martin.haring2@edu.fh-joanneum.at>
2016-12-06 11:56:23 +0100

    Version 1.1 DrawTriangle is now a one-liner

--- app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
+++ app/src/main/scala/at/fh/swengb/sierpinski_android/SierpinskiView.scala
@@ -23,23 +23,17 @@ class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View
   }
 
   def drawSierpinski(canvas: Canvas, x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float) = {
-    drawTriangle(canvas, x1, y1, x2, y2, x3, y3)
+    canvas.drawLines(Array(x1, y1, x2, y2, x2, y2, x3, y3, x3, y3, x1, y1), paint)
     drawSubTriangle(canvas, depth, (x1 + x2)/2, (y1 + y2)/2, (x1 + x3)/2, (y1 + y3)/2, (x2 + x3)/2, (y2 + y3)/2)
   }
 
   def drawSubTriangle(canvas: Canvas, depth: Int, x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float): Unit = {
     if (depth > 0){
-      drawTriangle(canvas, x1, y1, x2, y2, x3, y3)
+      canvas.drawLines(Array(x1, y1, x2, y2, x2, y2, x3, y3, x3, y3, x1, y1), paint)
       val newDepth = depth - 1
       drawSubTriangle(canvas, newDepth, (x1 + x2)/2 + (x2 - x3)/2, (y1 + y2)/2 + (y2 - y3)/2, (x1 + x2)/2 + (x1 - x3)/2, (y1 + y2)/2 + (y1 - y3)/2, (x1 + x2)/2, (y1 + y2)/2)
       drawSubTriangle(canvas, newDepth, (x3 + x2)/2 + (x2 - x1)/2, (y3 + y2)/2 + (y2 - y1)/2, (x3 + x2)/2 + (x3 - x1)/2, (y3 + y2)/2 + (y3 - y1)/2, (x3 + x2)/2, (y3 + y2)/2)
       drawSubTriangle(canvas, newDepth, (x1 + x3)/2 + (x3 - x2)/2, (y1 + y3)/2 + (y3 - y2)/2, (x1 + x3)/2 + (x1 - x2)/2, (y1 + y3)/2 + (y1 - y2)/2, (x1 + x3)/2, (y1 + y3)/2)
     }
-  }
-
-  def drawTriangle(canvas: Canvas, x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float) ={
-    canvas.drawLine(x1, y1, x2, y2, paint)
-    canvas.drawLine(x2, y2, x3, y3, paint)
-    canvas.drawLine(x3, y3, x1, y1, paint)
   }
 }
